89e308146566c2e9a96c1f46b319e1c12152071d
c9s <yoan@maicoin.com>
2020-02-07 18:33:46 +0800

    check the match result before we traverse the definitions

--- src/utils.js
+++ src/utils.js
@@ -386,23 +386,24 @@ export function optionsList(schema) {
 function findSchemaDefinition($ref, definitions = {}) {
   // Extract and use the referenced definition if we have it.
   const match = /^#\/definitions\/(.*)$/.exec($ref);
-  if (match && match[1]) {
-    const parts = match[1].split("/");
-    let current = definitions;
-    for (let part of parts) {
-      part = part.replace(/~1/g, "/").replace(/~0/g, "~");
-      if (current.hasOwnProperty(part)) {
-        current = current[part];
-      } else {
-        // No matching definition found, that's an error (bogus schema?)
-        throw new Error(`Could not find a definition for ${$ref}.`);
-      }
-    }
-    return current;
+
+  if (!match || match.length === 0) {
+    // No matching definition found, that's an error (bogus schema?)
+    throw new Error(`Could not find a definition for ${$ref}.`);
   }
 
-  // No matching definition found, that's an error (bogus schema?)
-  throw new Error(`Could not find a definition for ${$ref}.`);
+  const parts = match[1].split("/");
+  let current = definitions;
+  for (let part of parts) {
+    part = part.replace(/~1/g, "/").replace(/~0/g, "~");
+    if (current.hasOwnProperty(part)) {
+      current = current[part];
+    } else {
+      // No matching definition found, that's an error (bogus schema?)
+      throw new Error(`Could not find a definition for ${$ref}.`);
+    }
+  }
+  return current;
 }
 
 // In the case where we have to implicitly create a schema, it is useful to know what type to use
@@ -468,7 +469,7 @@ export function resolveSchema(schema, definitions = {}, formData = {}) {
 
 function resolveReference(schema, definitions, formData) {
   // Retrieve the referenced schema definition.
-  const $refSchema = findSchemaDefinition(schema.$ref, definitions);
+    const $refSchema = findSchemaDefinition(schema.$ref, definitions);
   // Drop the $ref property of the source schema.
   const { $ref, ...localSchema } = schema;
   // Update referenced schema definition with local schema properties.
